002ba1336f3441e9008339d19cdb1a9ff11e4c73
fsdg129 <yaozuw@gmail.com>
2020-07-02 18:58:34 -0500

    Update App

--- install-packages.bat
+++ install-packages.bat
@@ -1,3 +1,4 @@
 npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/preset-react
 npm install --save-dev webpack webpack-cli webpack-dev-server style-loader css-loader babel-loader
 npm install --save-dev react react-dom
+npm install react-hot-loader
