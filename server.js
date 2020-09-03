15f4f18763ba6808a1573a8cf140db0bc9bb1fa7
fsdg129 <yaozuw@gmail.com>
2020-09-02 22:39:26 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const app = express();
+const fs = require('fs');
 
-const PORT = process.env.PORT || 3000;
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
+const MODE = process.env.MODE || "LOCAL";
+
+var options = {
+	key: fs.readFileSync('./static/frontendkey.pem'),
+	cert: fs.readFileSync('./static/frontendcert.pem')
+  };
+
+app.use((req, res, next) => {
+
+	if (req.secure || req.headers['x-forwarded-proto'] == 'https'){
+		return next();
+	} else{
+		if(MODE == 'LOCAL'){
+			return res.redirect('https://localhost:3443' + req.url);
+		} else{
+			return res.redirect('https://' + req.headers.host + req.url);
+		}
+	}
+
+});
 
 app.get('/bundle.js', function( req, res ) {
 	res.sendFile(__dirname+"/dist/bundle.js");
 });
 
 app.get('/*', function( req, res ) {
-	res.sendFile(__dirname+"/index.html");
+	res.sendFile(__dirname+"/static/index.html");
 });
 
-http.listen( PORT, function() {
-    console.log( 'listening on *:' + PORT );
+http.createServer(app).listen( HTTP_PORT, function() {
+    console.log( 'http listening on *:' + HTTP_PORT );
+});
+
+https.createServer(options, app).listen( HTTPS_PORT, function() {
+    console.log( 'https listening on *:' + HTTPS_PORT );
 });
